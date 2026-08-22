b8084072d4f7f59a9cd883fe7e4ad461e9455131
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-22 10:58:44 +0300

    Make story progress child-scoped and prevent repeat reward farming

--- lib/core/stories/story_progress_service_v20.dart
+++ lib/core/stories/story_progress_service_v20.dart
@@ -1,20 +1,11 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import '../storage/child_progress_repository.dart';
 
 class StoryProgressServiceV20 {
   static Future<void> complete(
     String id, {
     required int questionsCorrect,
-  }) async {
-    final p = await SharedPreferences.getInstance();
-    await p.setBool('story.$id.completed', true);
-    await p.setInt('story.$id.score', questionsCorrect);
-    await p.setInt(
-      'child_stars_v15',
-      (p.getInt('child_stars_v15') ?? 0) + questionsCorrect,
-    );
-    await p.setInt(
-      'child_xp_v15',
-      (p.getInt('child_xp_v15') ?? 0) + questionsCorrect * 5,
-    );
-  }
+  }) => ChildProgressRepository.completeStory(id, questionsCorrect);
+
+  static Future<bool> completed(String id) =>
+      ChildProgressRepository.storyCompleted(id);
 }
