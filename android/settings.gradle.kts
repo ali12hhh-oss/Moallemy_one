52530cbfd624ed5a0a97c962cc5b6b2f7a0f59cb
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-23 03:05:14 +0300

    chore(android): upgrade AGP and Kotlin to supported versions

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
     val flutterSdkPath =
         run {
@@ -18,11 +19,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
@@ -30,5 +33,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "daleel_child"
 include(":app")
