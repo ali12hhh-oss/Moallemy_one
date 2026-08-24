789e3aa3765f983e2812fc690ca8f688548e3f99
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-24 05:58:08 +0300

    Pass purchased title to child store state

--- lib/core/store/store_service_v23.dart
+++ lib/core/store/store_service_v23.dart
@@ -3,8 +3,8 @@ import '../storage/child_progress_repository.dart';
 class StoreServiceV23 {
   static Future<int> stars() => ChildProgressRepository.stars();
 
-  static Future<bool> buy(String id, int price) =>
-      ChildProgressRepository.buy(id, price);
+  static Future<bool> buy(String id, int price, {String? title}) =>
+      ChildProgressRepository.buy(id, price, title: title);
 
   static Future<bool> owned(String id) => ChildProgressRepository.owned(id);
 }
