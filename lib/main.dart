1c8876d8e39393bc5cd898a7d036ac1c8258a713
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-29 09:02:22 +0300

    Preload welcome audio during app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'core/audio/voice_service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  unawaited(VoiceService.preloadWelcome());
   runApp(const DaleelChildApp());
 }
