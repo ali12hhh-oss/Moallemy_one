277c394c6b481d4ae1612f4eebb7506415af70df
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-29 18:31:00 +0300

    Apply selected store backgrounds to section screens

--- lib/widgets/section_background.dart
+++ lib/widgets/section_background.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'store_background.dart';
+
 class SectionBackground extends StatelessWidget {
   final String asset;
   final Widget child;
 
-  const SectionBackground({super.key, required this.asset, required this.child});
+  const SectionBackground({
+    super.key,
+    required this.asset,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Image.asset(asset, fit: BoxFit.cover),
-        Container(color: Colors.black.withValues(alpha: .06)),
-        child,
-      ],
+    return StoreBackground(
+      originalAsset: asset,
+      overlayColor: Colors.black.withValues(alpha: .06),
+      child: child,
     );
   }
 }
