4a2eaddba9d40fc1beaa8a8202875f2c7eeb54a5
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-31 21:40:44 +0300

    Remove startup welcome audio preload

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
-import 'core/audio/voice_service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  unawaited(VoiceService.preloadWelcome());
   runApp(const DaleelChildApp());
 }
