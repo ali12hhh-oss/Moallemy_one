812259df981472ec3f13f66743863729807d043e
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-06 00:31:26 +0300

    fix: use Gradle distribution installed by CI

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,21 +1,23 @@
+@rem Gradle startup script for Windows
 @echo off
-setlocal
-set "APP_HOME=%~dp0"
-set "WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar"
-set "WRAPPER_URL=https://services.gradle.org/distributions/gradle-8.14-wrapper.jar"
 
-if not exist "%WRAPPER_JAR%" (
-  echo Downloading Gradle wrapper jar...
-  powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -UseBasicParsing -Uri '%WRAPPER_URL%' -OutFile '%WRAPPER_JAR%'"
-  if errorlevel 1 exit /b 1
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_HOME=%DIRNAME%
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+if not exist "%CLASSPATH%" (
+    echo ERROR: Gradle wrapper JAR is missing: %CLASSPATH% 1>&2
+    echo Run "gradle wrapper --gradle-version 8.14" locally and commit the generated wrapper JAR. 1>&2
+    exit /b 1
 )
 
-if not defined JAVA_HOME (
-  set "JAVA_EXE=java.exe"
+if defined JAVA_HOME (
+    set JAVACMD=%JAVA_HOME%\bin\java.exe
 ) else (
-  set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
+    set JAVACMD=java.exe
 )
 
-"%JAVA_EXE%" %JAVA_OPTS% %GRADLE_OPTS% -classpath "%WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
-set "EXIT_CODE=%ERRORLEVEL%"
-endlocal & exit /b %EXIT_CODE%
+"%JAVACMD%" %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
