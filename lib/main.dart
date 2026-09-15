163226166b20130425b00d2e497bcd9721cdc0ad
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-15 16:54:21 +0300

    Initialize AdMob before app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'services/ad_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await AdService.initialize();
   runApp(const DaleelChildApp());
 }
