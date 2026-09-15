6c086aa29f2d9a3a8f5c7325beb71cd59581457b
ali12hhh-oss <ali12.hhh@gmail.com>
2026-09-15 17:02:13 +0300

    add reusable banner and native AdMob widgets

---
+++ lib/services/ad_widgets.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import 'ad_service.dart';
+
+class AdBanner extends StatefulWidget {
+  const AdBanner({super.key, this.margin = const EdgeInsets.symmetric(vertical: 12)});
+
+  final EdgeInsets margin;
+
+  @override
+  State<AdBanner> createState() => _AdBannerState();
+}
+
+class _AdBannerState extends State<AdBanner> {
+  BannerAd? _ad;
+  bool _loaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _ad = BannerAd(
+      adUnitId: AdService.bannerAdUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          if (mounted) setState(() => _loaded = true);
+        },
+        onAdFailedToLoad: (ad, _) {
+          ad.dispose();
+          if (mounted) setState(() => _loaded = false);
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _ad?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_loaded || _ad == null) return const SizedBox.shrink();
+    return Container(
+      margin: widget.margin,
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: _ad!.size.width.toDouble(),
+        height: _ad!.size.height.toDouble(),
+        child: AdWidget(ad: _ad!),
+      ),
+    );
+  }
+}
+
+class NativeAdCard extends StatefulWidget {
+  const NativeAdCard({super.key, this.margin = const EdgeInsets.symmetric(vertical: 12)});
+
+  final EdgeInsets margin;
+
+  @override
+  State<NativeAdCard> createState() => _NativeAdCardState();
+}
+
+class _NativeAdCardState extends State<NativeAdCard> {
+  NativeAd? _ad;
+  bool _loaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _ad = NativeAd(
+      adUnitId: AdService.nativeAdUnitId,
+      request: const AdRequest(),
+      nativeTemplateStyle: NativeTemplateStyle(
+        templateType: TemplateType.small,
+        cornerRadius: 14,
+        mainBackgroundColor: Colors.white,
+      ),
+      listener: NativeAdListener(
+        onAdLoaded: (ad) {
+          if (mounted) setState(() => _loaded = true);
+        },
+        onAdFailedToLoad: (ad, _) {
+          ad.dispose();
+          if (mounted) setState(() => _loaded = false);
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _ad?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_loaded || _ad == null) return const SizedBox.shrink();
+    return Container(
+      margin: widget.margin,
+      constraints: const BoxConstraints(minHeight: 100),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(14),
+        child: AdWidget(ad: _ad!),
+      ),
+    );
+  }
+}
